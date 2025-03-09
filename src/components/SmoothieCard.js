70bce1f3a50e0001472a697804e0ac6dd211f410
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-03-09 10:07:51 +0530

    feat: add edit link to SmoothieCard and display updating item ID in Update page

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
+
 const smoothieCard = ({ smoothie }) => {
   return (
     <div className="smoothie-card">
       <h2>{smoothie.title}</h2>
       <p>{smoothie.method}</p>
       <div className="rating">{smoothie.rating}</div>
+      <div className="buttons">
+        <Link to={"/" + smoothie.id}>
+          <i className="material-icons">edit</i>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,9 +1,12 @@
+import { useParams } from "react-router-dom";
 const Update = () => {
+  const { id } = useParams();
   return (
     <div className="page update">
       <h2>Update</h2>
+      <p>Updating item with ID: {id}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Update
+export default Update;
